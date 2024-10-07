37ffc1c4e334f0b17d259e45c34b1e47d47fa351
Jukoss <tusyukv@gmail.com>
2024-10-07 22:54:54 +0300

    referral form markup

---
+++ public/js/referral-form.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('select').multipleSelect(
+        {
+            selectAll: false,
+            width: "100%",
+            placeholder: "Choose...",
+        }
+    );
+});
